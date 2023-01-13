4edc30a4195c141509adaf3b9b52828986edafb0
Mubarek Mohammed <38358880+mube1@users.noreply.github.com>
2023-01-13 19:18:19 +0300

    Add files via upload

---
+++ password_generator_v2.py
@@ -0,0 +1,45 @@
+import pickle,sys
+import numpy as np
+
+with open('normalized_main_stats.pkl', 'rb') as d:
+    """ Read trained pkl file of updated dictionaries to calculate probabilites from"""
+    main_stats = pickle.load(d)
+
+def character_generator(ngrams):
+    """ this function takes in a n characters (n-grams) and generates the next char. 
+        If the n gram is not in the list, checks a smaller section of it.
+    """
+    if ngrams in main_stats:
+        return np.random.choice(list(main_stats[ngrams].keys()), p=list(main_stats[ngrams].values()) )
+    else:
+        return character_generator(ngrams[:-1])
+        
+        
+def password(length,gram=4):
+    """
+    This function takes in a length of the password to be generated, then generates it character by character based on the stats dictionary
+    """
+    password = '`' * gram
+    i=0
+    while len(password.replace('`', '')) <length:
+        password+=character_generator(password[i:i+gram])
+        i+=1
+    return password.replace('`', '')
+
+
+def generate_n(number_of_passwords,length=8):
+    passwords=[]
+    for i in range(number_of_passwords):
+        passwords.append(password(length))
+    return passwords
+
+def main():
+    """ This function parses the command line arguments and calls the generator function"""
+    length=int(sys.argv[1])
+    number_passwords=int(sys.argv[2])
+    passwords=generate_n(number_passwords,length)
+    print('\n'.join(passwords))
+    
+
+if __name__ == "__main__":
+    main()
